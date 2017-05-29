ab6c203a627918f8174f798624a9f01c948cfc30
nikhilakki <nikhilakki@users.noreply.github.com>
2017-05-29 15:35:27 +0000

    Done

--- build.py
+++ build.py
@@ -3,7 +3,10 @@ def solution(dic):
     '''
     Enter your code here
     '''
+    last_index_of_target = dic['k1'][3]['tricky'][3]['target'][3]
     return last_index_of_target
 
 
 dic = {'k1': [1, 2, 3, {'tricky': ['oh', 'man', 'inception', {'target': [1, 2, 3, 'hello']}]}]}
+
+print(solution(dic))
