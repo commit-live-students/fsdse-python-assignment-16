ff43b33517ddc518acd3ff1ffa27864d31d58d5a
sharduln <sharduln@gmail.com>
2017-06-03 07:27:59 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,14 @@
 def solution(dic):
-    last_index_of_target = None
     '''
     Enter your code here
     '''
-    return last_index_of_target
+    if 'target' in dic:
+        return dic['target'][-1]
+    for key in dic:
+        if isinstance(dic[key], list):
+            for item in dic[key]:
+                if isinstance(item,dict):
+                    return solution(item)
 
 
 dic = {'k1': [1, 2, 3, {'tricky': ['oh', 'man', 'inception', {'target': [1, 2, 3, 'hello']}]}]}
