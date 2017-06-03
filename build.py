6f3b52377eb0806c09e9643a6aaf07f575a8f2b7
Rohan2015 <rohandamodar@outlook.com>
2017-06-03 08:43:01 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,36 @@
 def solution(dic):
     last_index_of_target = None
-    '''
-    Enter your code here
-    '''
+    #temp_list = []
+
+    for value in dic.values():
+	if isinstance(value,list):
+		temp_list = value
+    #print temp_list
+
+    for i in temp_list:
+        if isinstance(i,dict):
+	    temp_dict = i
+    #print temp_dict
+
+    for value in temp_dict.values():
+    	if isinstance(value,list):
+	    temp_list = value
+    #print temp_list
+
+    for i in temp_list:
+    	if isinstance(i,dict):
+            temp_dict = i
+    #print temp_dict
+
+    for value in temp_dict.values():
+        if isinstance(value,list):
+            temp_list = value
+    #print temp_list
+
+    for i in temp_list:
+        last_index_of_target = temp_list[len(temp_list)-1]
     return last_index_of_target
+    #return last_index_of_target
 
 
 dic = {'k1': [1, 2, 3, {'tricky': ['oh', 'man', 'inception', {'target': [1, 2, 3, 'hello']}]}]}
