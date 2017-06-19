c8efdb5ccac4ea8c25a130f21da34dc3079f12ac
kaushikb11 <kaushikbokka@gmail.com>
2017-06-19 09:26:18 +0000

    Done

--- build.py
+++ build.py
@@ -1,8 +1,6 @@
 def solution(dic):
     last_index_of_target = None
-    '''
-    Enter your code here
-    '''
+    last_index_of_target=dic['k1'][3]['tricky'][3]['target'][3]
     return last_index_of_target
 
 
