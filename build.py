97057b143c0715dc86dea8057e2f51fcacb18eda
ramkrishan-sahu <ram04190@gmail.com>
2017-06-24 07:30:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,14 @@
 def solution(dic):
-    last_index_of_target = None
-    '''
-    Enter your code here
-    '''
-    return last_index_of_target
+    l1 = dic.values()
+    print l1[0]
+    l2 = l1[0][3]
+    l3 = l2.values()
+    l4 = l3[0]
+    l5 = l4[3].values()
+    l6 = l5[0]
+    l7 = l6[3]
+    print l7
+    return l7
 
 
 dic = {'k1': [1, 2, 3, {'tricky': ['oh', 'man', 'inception', {'target': [1, 2, 3, 'hello']}]}]}
