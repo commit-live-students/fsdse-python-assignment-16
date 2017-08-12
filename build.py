a9dd4ddd4fa72debd8776b80eca2855d5052aa02
SiddharthPant <spsiddharthpant@gmail.com>
2017-08-12 15:41:34 +0000

    Done

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 def solution(dic):
     last_index_of_target = None
-    '''
-    Enter your code here
-    '''
-    return last_index_of_target
+    if type(dic) is dict:
+        a_list = dic.itervalues().next()
+    else:
+        a_list = dic
+    if type(a_list) is list:
+        return solution(a_list[-1])
+
+    return a_list
 
 
 dic = {'k1': [1, 2, 3, {'tricky': ['oh', 'man', 'inception', {'target': [1, 2, 3, 'hello']}]}]}
